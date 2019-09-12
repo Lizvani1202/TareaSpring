c70b6bd11dd65ebce4e39ee103d9a02727f53608
LizbethVera <lizveraduran@gmail.com>
2019-09-12 01:46:04 -0400

    Segunda Parte

---
+++ src/main/java/Agenda/agendaOdontologo.java
@@ -0,0 +1,10 @@
+package Agenda;
+
+import Modelo.Odontologo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface agendaOdontologo extends JpaRepository<Odontologo, Long>{
+
+}
+
+
---
+++ src/main/java/Agenda/solicitudAgenda.java
@@ -0,0 +1,12 @@
+package Agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class solicitudAgenda {
+
+    public static void main(String[] args ){
+        SpringApplication.run(solicitudAgenda.class, args);
+        
+    }
+}
